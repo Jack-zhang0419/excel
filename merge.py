099bab184eb896750a3871f7b04b72fd57b6db94
Xie, Liang-Chang <neo.xie@ksb.com>
2021-01-09 11:34:54 +0800

    enhanced

--- merge.py
+++ merge.py
@@ -7,21 +7,21 @@ def merge(sub_dir):
     docstring
     """
     print('================================================================')
-    print(f'start to merge {sub_dir} ...')
+    print(f'start to merge {sub_dir} ......')
     # 文件路径
     # 构建新的表格名称
     merged_file = sub_dir + '/merged.xlsx'
     # 找到文件路径下的所有表格名称，返回列表
-    file_list = os.listdir(sub_dir)
     new_list = []
 
-    for file in file_list:
-        if file != 'merged.xlsx' and (file.endswith('.xls')
-                                      or file.endswith('.xlsx')):
+    filter_list = [x for x in os.listdir(sub_dir) if x != 'merged.xlsx']
+
+    for file in filter_list:
+        if file.endswith('.xls') or file.endswith('.xlsx'):
             # 重构文件路径
             file_path = os.path.join(sub_dir, file)
             # 将excel转换成DataFrame
-            print(file_path)
+            print(f'merging {file_path} ...')
             dataframe = pd.read_excel(file_path)
             # 保存到新列表中
             new_list.append(dataframe)
@@ -37,11 +37,7 @@ def main():
     inputFolder = f"{os.getcwd()}/input/"
     # print(inputFolder)
     subdirs = []
-    for subdir in [x[0] for x in os.walk(inputFolder)]:
-        if subdir != inputFolder:
-            subdirs.append(subdir)
-
-    for subdir in subdirs:
+    for subdir in [x[0] for x in os.walk(inputFolder) if x[0] != inputFolder]:
         merge(subdir)
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,15 @@
 # readme
 
+## prepare
+
+make sure to create folder "input" in this folder, and put "ready to merge excel files" under "input folder" in subfolder.
+
+```sh
+pip install -r requirements.txt
+
+python merge.py
+```
+
 ## reference link
 
 - [python合并多个EXCEL表](https://www.jianshu.com/p/664b52d6933e)
